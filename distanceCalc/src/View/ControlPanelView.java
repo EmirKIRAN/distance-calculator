95a9033c91028c5b99c53fc3320d8cb27650a944
Emirhan KIRAN <62514552+EmirKIRAN@users.noreply.github.com>
2021-10-20 22:01:19 +0300

    added top section to JFrame

--- distanceCalc/src/View/ControlPanelView.java
+++ distanceCalc/src/View/ControlPanelView.java
@@ -39,6 +39,7 @@ public class ControlPanelView extends JFrame{
 		this.setLayout(null);
 		this.setResizable(false);
 		
+		this.initFrameSection();
 		this.getContentPane().setBackground(this.classicBackgroundColor);
 	}
 	
@@ -46,5 +47,69 @@ public class ControlPanelView extends JFrame{
 	{
 		this.setVisible(true);
 	}
+	
+	private void initFrameSection()
+	{
+		JPanel framePanel = new JPanel(null);
+		framePanel.setBounds(0,0,900,500);
+		framePanel.setBackground(this.classicBackgroundColor);
+		
+		float centerPosition = (this.WIDTH/2) - (800/2);
+		
+		this.isLive = new JLabel();
+		this.isLive.setText("LIVE");
+		this.isLive.setForeground(Color.RED);
+		this.isLive.setBounds(750,25,250,40);
+		this.isLive.setIconTextGap(8);
+		this.isLive.setFont(new Font("Arial", Font.BOLD, 18));
+		this.isLive.setIcon(new ImageIcon("./images/icons/record.png"));
+		framePanel.add(this.isLive);
+		
+		this.imageFrame = new JLabel();
+		this.imageFrame.setBounds((int)centerPosition,20,800,400);
+		this.imageFrame.setIcon(new ImageIcon("/home/emir/Masaüstü/work/Deep Learning/ship-classifier/models/test-data/cargo/2692333.jpg"));
+		this.imageFrame.setBorder(BorderFactory.createMatteBorder(2, 2, 4, 2, new Color(75, 101, 132)));
+		framePanel.add(this.imageFrame);
+		
+		JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.LEADING,10,20));
+		bottomPanel.setBounds((int)centerPosition, 425, 800,70);
+		bottomPanel.setBackground(this.classicBackgroundColor);
+		
+		this.selectImage = new JRadioButton("NORMAL IMAGE");
+		this.selectImage.setFocusable(false);
+		this.selectImage.setPreferredSize(new Dimension(150,40));
+		this.selectImage.setBackground(this.classicBackgroundColor);
+		this.selectImage.setSelected(true);
+		
+		this.selectMask = new JRadioButton("MASKED IMAGE");
+		this.selectMask.setFocusable(false);
+		this.selectMask.setPreferredSize(new Dimension(150,40));
+		this.selectMask.setBackground(this.classicBackgroundColor);
+		
+		this.startCapture = new JButton("START CAPTURE");
+		this.startCapture.setPreferredSize(new Dimension(200,45));
+		this.startCapture.setIcon(new ImageIcon("./images/icons/radar.png"));
+		this.startCapture.setIconTextGap(15);
+		this.startCapture.setFocusable(false);
+		
+		this.stopCapture = new JButton("STOP CAPTURE");
+		this.stopCapture.setPreferredSize(new Dimension(200,45));
+		this.stopCapture.setIcon(new ImageIcon("./images/icons/signal.png"));
+		this.stopCapture.setIconTextGap(15);
+		this.stopCapture.setFocusable(false);
 
+		
+		ButtonGroup buttonGroup = new ButtonGroup();
+		buttonGroup.add(this.selectImage);
+		buttonGroup.add(this.selectMask);
+		
+		bottomPanel.add(this.selectImage);
+		bottomPanel.add(this.selectMask);
+		bottomPanel.add(startCapture);
+		bottomPanel.add(this.stopCapture);
+		
+		framePanel.add(bottomPanel);
+		this.add(framePanel);
+	}
+	
 }
