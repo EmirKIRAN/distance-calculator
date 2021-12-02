101b88717efe89a3199daa077ab47bcb770e5258
Emirhan KIRAN <62514552+EmirKIRAN@users.noreply.github.com>
2021-12-02 20:50:58 +0300

    updated Main class

--- distanceCalc/src/Main/Main.java
+++ distanceCalc/src/Main/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		ControlPanelView panel = new ControlPanelView("Distance App", "./images/icons/dialog.png");
 		panel.showWindow();
 	}
