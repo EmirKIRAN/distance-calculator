dc531eb6ca4aebb3fe1f97034fb77657eed229c7
Emirhan KIRAN <62514552+EmirKIRAN@users.noreply.github.com>
2021-12-02 20:46:14 +0300

    create Camera controller class

---
+++ distanceCalc/src/Controller/CameraController.java
@@ -0,0 +1,16 @@
+package Controller;
+
+import javax.swing.JLabel;
+
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.videoio.VideoCapture;
+
+import Helper.Converter;
+import Helper.Preprocessing;
+
+public class CameraController {
+	
+	
+
+}
