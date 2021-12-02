dd29658f4b49ba094cbdd83f36264c09f2cf59f7
Emirhan KIRAN <62514552+EmirKIRAN@users.noreply.github.com>
2021-12-02 20:48:29 +0300

    create Settings controller class

---
+++ distanceCalc/src/Controller/SettingsController.java
@@ -0,0 +1,21 @@
+package Controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import Helper.ProgressDialog;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JSlider;
+
+public class SettingsController {
+
+}
